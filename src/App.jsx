8d37a5a538da7c0d13f879957bdfd790ba2db4d6
mguomanila <12101668+mguomanila@users.noreply.github.com>
2022-07-06 07:32:49 +0800

    Add sass

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,21 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import './App.scss'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
+      <div className="App-header">
+        <p>Wallets with ASK</p>
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
+          Track and manage your ASK holdings across all of the wallets that you use.
         </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      </div>
+      <div className="main">
+
+      </div>
+      <div></div>
     </div>
-  )
+  );
 }
 
 export default App
