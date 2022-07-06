575312dc432da80a79089b7436de21ab610f257d
mguomanila <12101668+mguomanila@users.noreply.github.com>
2022-07-06 10:18:03 +0800

    Add moralis sdk

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
+import React from 'react'
 import './App.scss'
 
 function App() {
+  const [totalBalance, setTotalBalance] = React.useState(0)
 
   return (
     <div className="App">
@@ -10,9 +12,18 @@ function App() {
           Track and manage your ASK holdings across all of the wallets that you use.
         </p>
       </div>
-      <div className="main">
-
-      </div>
+      <section className="main">
+        <div>
+          <p>ASK</p>
+          <p>Total Balance:</p>
+          {totalBalance}
+        </div>
+        <form>
+          <p>Welcome to ASK Wallet Page.</p>
+          <p>Please enter a wallet address:</p>
+          <input name="Wallet"/>
+        </form>
+      </section>
       <div></div>
     </div>
   );
