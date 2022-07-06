c49e04640cfe97287e9d6066f775a27d9afc546d
mguomanila <12101668+mguomanila@users.noreply.github.com>
2022-07-06 18:16:05 +0800

    Fix layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,29 @@
 import React from 'react'
-import { Card, } from 'antd'
+import { Card,  } from 'antd'
+import { QrcodeOutlined, } from '@ant-design/icons'
 import Address from './components/Address'
 import './App.scss'
 
 const styles = {
   title: {
-    fontSize: "30px",
+    fontSize: "18px",
     fontWeight: "600",
   },
   header: {
     display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
+    "flexDirection": "column",
     gap: "5px",
   },
   card: {
     boxShadow: "0 0.5rem 1.2rem rgb(189 197 209 / 20%)",
     border: "1px solid #e7eaf3",
     borderRadius: "1rem",
-    width: "450px",
-    fontSize: "16px",
-    fontWeight: "500",
+    width: "100%",
+    fontSize: "15px",
+    fontWeight: "400",
+    flex: 'auto',
   },
-};
+}
 
 function App() {
   const [totalBalance, setTotalBalance] = React.useState(0)
@@ -42,36 +43,53 @@ function App() {
         }
       }
     )
-    console.log(resp)
-    setTotalBalance((resp as any).balance)
+    const json = await resp.json()
+    console.log(json)
+    setTotalBalance((json as any).balance)
   }
 
   return (
     <div className="App">
       <div className="App-header">
-        <p>Wallets with ASK</p>
-        <p>
-          Track and manage your ASK holdings across all of the wallets that you
-          use.
-        </p>
+        <Card style={styles.header}
+          title={
+            <div style={styles.title}>Wallets with ASK</div>
+          }
+        >
+          <p>
+            Track and manage your ASK holdings across all of the wallets that you
+            use.
+          </p>
+        </Card>
       </div>
-      <Card>
-        <p>ASK</p>
-        <p>Total Balance:</p>
-        {totalBalance}
-      </Card>
-      <Card>
-        <p>Welcome to ASK Wallet Page.</p>
-        <p>Please enter a wallet address:</p>
-        <input
-          name="Wallet"
-          ref={input}
-          value={address}
-          onChange={addressHandler}
-        />
-      </Card>
-      <Card style={styles.card}>
-        <Address address={address} size={6} copyable />
+      <div className="App-header">
+          <Card style={styles.card}>
+            <p style={styles.title}>ASK</p>
+            <p>Total Balance:</p> {totalBalance}
+          </Card>
+          <Card style={styles.card}>
+            <p style={styles.title}>Welcome to ASK Wallet Page.</p>
+            <p>Please enter a wallet address:</p>
+            <input
+              name="Wallet"
+              ref={input}
+              value={address}
+              onChange={addressHandler}
+            />
+          </Card>
+      </div>
+      <Card style={styles.card} className="QR-code">
+        <div>
+          <p style={styles.title}>Receive ASK</p>
+          <p>ERC-20</p>
+          <Address address={address} size={6} copyable />
+        </div>
+        <div>
+          <p>Share this QR code or public key with whomever is sending you ASK</p>
+        </div>
+        <div>
+          <QrcodeOutlined />
+        </div>
       </Card>
     </div>
   )
