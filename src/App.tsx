eba27f7c092bb7e9393fba56297d8e8bed727886
mguomanila <12101668+mguomanila@users.noreply.github.com>
2022-07-06 16:04:40 +0800

    Fix api url

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,47 @@ import { Card, } from 'antd'
 import Address from './components/Address'
 import './App.scss'
 
+const styles = {
+  title: {
+    fontSize: "30px",
+    fontWeight: "600",
+  },
+  header: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    gap: "5px",
+  },
+  card: {
+    boxShadow: "0 0.5rem 1.2rem rgb(189 197 209 / 20%)",
+    border: "1px solid #e7eaf3",
+    borderRadius: "1rem",
+    width: "450px",
+    fontSize: "16px",
+    fontWeight: "500",
+  },
+};
+
 function App() {
   const [totalBalance, setTotalBalance] = React.useState(0)
   const [address, setAddress] = React.useState('')
-  const input = React.useRef()
+  const input = React.useRef<HTMLInputElement>()
 
-  function addressHandler(e) {
+  async function addressHandler(_e: any) {
     setAddress(input.current.value)
+    const url = `${process.env.API_URL}/${input.current.value}/balance?chain=eth`
+    console.log(`url:${url}`)
+    const resp = await fetch(
+      url,
+      {
+        headers: {
+          accept: 'application/json',
+          'X-API-Key': process.env.X_API_KEY
+        }
+      }
+    )
+    console.log(resp)
+    setTotalBalance((resp as any).balance)
   }
 
   return (
@@ -36,8 +70,8 @@ function App() {
           onChange={addressHandler}
         />
       </Card>
-      <Card>
-        <Address address={address} />
+      <Card style={styles.card}>
+        <Address address={address} size={6} copyable />
       </Card>
     </div>
   )
